df18f6d563c5195dc334a46cf06f1d70070d8f11
Haitomns G <haitomns4173@gmail.com>
2025-09-26 07:42:39 +0000

    Try fixing this error: `Console Error: A param property was accessed dir

--- src/app/shop/[id]/page.tsx
+++ src/app/shop/[id]/page.tsx
@@ -4,7 +4,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
-import { useState } from "react";
+import { useState, use } from "react";
 import { ArrowRight, Star } from "lucide-react";
 
 import { products } from "@/lib/data";
